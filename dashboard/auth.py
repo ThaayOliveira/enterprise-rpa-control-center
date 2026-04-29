7309d973ca9e47d71e5a025ca3657c4db838f8cc
Thayanne Oliveira <jobsthay@gmail.com>
2026-04-29 11:34:44 -0300

    fix: report bot bugs

--- dashboard/auth.py
+++ dashboard/auth.py
@@ -1,21 +1,9 @@
 import streamlit as st
-
 from services import login, refresh_token
 from login_styles import load_login_styles
-from streamlit_cookies_manager import EncryptedCookieManager
-
-
-cookies = EncryptedCookieManager(
-    prefix="rpa_",
-    password="senha-super-secreta-123"
-)
-
-if not cookies.ready():
-    st.stop()
 
 
 def init_session():
-
     if "authenticated" not in st.session_state:
         st.session_state.authenticated = False
 
@@ -27,22 +15,12 @@ def init_session():
 
 
 def logout():
-
     st.session_state.authenticated = False
     st.session_state.token = None
     st.session_state.refresh_token = None
 
-    if "token" in cookies:
-        del cookies["token"]
-
-    if "refresh_token" in cookies:
-        del cookies["refresh_token"]
-
-    cookies.save()
-
 
 def render_login():
-
     load_login_styles()
 
     st.markdown(
@@ -60,65 +38,26 @@ def render_login():
     col1, col2, col3 = st.columns([1, 1.2, 1])
 
     with col2:
+        username = st.text_input("User Name")
+        password = st.text_input("Password", type="password")
 
-        username = st.text_input(
-            "User Name",
-            placeholder="Enter your username"
-        )
-
-        password = st.text_input(
-            "Password",
-            type="password",
-            placeholder="Enter Password"
-        )
-
-        st.markdown(
-            "<div class='forgot'>FORGOT PASSWORD?</div>",
-            unsafe_allow_html=True
-        )
-
-        if st.button(
-            "SIGN IN",
-            width="stretch"
-        ):
-
+        if st.button("SIGN IN", use_container_width=True):
             resp = login(username, password)
 
             if resp.status_code == 200:
-
                 data = resp.json()
 
-                access = data["access_token"]
-                refresh = data["refresh_token"]
-
-                st.session_state.token = access
-                st.session_state.refresh_token = refresh
+                st.session_state.token = data["access_token"]
+                st.session_state.refresh_token = data["refresh_token"]
                 st.session_state.authenticated = True
 
-                cookies["token"] = access
-                cookies["refresh_token"] = refresh
-                cookies.save()
-
                 st.rerun()
-
             else:
                 st.error("Invalid credentials")
 
-        st.markdown(
-            "<div class='signup'>Don't have an account? <b>Sign up</b></div>",
-            unsafe_allow_html=True
-        )
-
 
 def require_login():
-
     init_session()
-
-    if cookies.get("token") and not st.session_state.authenticated:
-
-        st.session_state.token = cookies["token"]
-        st.session_state.refresh_token = cookies.get("refresh_token")
-        st.session_state.authenticated = True
 
     if not st.session_state.authenticated:
         render_login()
@@ -126,21 +65,11 @@ def require_login():
 
 
 def refresh_access_token():
-
-    resp = refresh_token(
-        st.session_state.refresh_token
-    )
+    resp = refresh_token(st.session_state.refresh_token)
 
     if resp.status_code == 200:
-
         data = resp.json()
-
-        new_token = data["access_token"]
-
-        st.session_state.token = new_token
-        cookies["token"] = new_token
-        cookies.save()
-
+        st.session_state.token = data["access_token"]
         return True
 
     logout()
